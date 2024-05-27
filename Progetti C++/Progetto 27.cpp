283d4fd57649514c409a34817ea710f0b8d895b5
Alessandro Carloni <carloni.a2006@gmail.com>
2024-05-27 08:36:59 +0000

    Prove in Classe

---
+++ Progetti C++/Progetto 27.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    cout << "Benvenuto su GitPod!" << endl;
+    string name;
+    cout << "Inserisci il tuo nome: " << endl;
+    cin >> name;
+    cout << "Ciao " << name << endl;
+
+
+    return 0;
+}
